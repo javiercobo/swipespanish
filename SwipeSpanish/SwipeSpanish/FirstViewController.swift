1408bf124ed94bb669814780d5b7ec24bc472931
Javi Cobo <yocobo@gmail.com>
2015-12-13 13:08:28 -0500

    Starting API

---
+++ SwipeSpanish/SwipeSpanish/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  SwipeSpanish
+//
+//  Created by Cobo, Javier on 12/12/15.
+//  Copyright © 2015 Cobo, Javier. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwipeSpanish/SwipeSpanish/FlickrPhoto.swift
@@ -0,0 +1,24 @@
+//
+//  FlickrPhoto.swift
+//  SwipeSpanish
+//
+//  Created by Cobo, Javier on 12/12/15.
+//  Copyright © 2015 Cobo, Javier. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhoto: NSObject {
+
+    var thumbnail:UIImage!
+    var largeImage:UIImage!
+    
+    var photoID:String!
+    var farm:Int!
+    var server:String!
+    var secret:String!
+    
+    override init(){
+        
+    }
+}
